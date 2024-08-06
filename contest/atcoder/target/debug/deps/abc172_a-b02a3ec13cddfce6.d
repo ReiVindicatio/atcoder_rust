fb38e477ac4ec0b0f4054d6a0843601ae44ce68f
斎藤理人 <MasatoSaito@MacBook-Air.local>
2024-08-06 21:22:00 +0900

    a

---
+++ contest/atcoder/target/debug/deps/abc172_a-b02a3ec13cddfce6.d
@@ -0,0 +1,5 @@
+/Users/MasatoSaito/competitive_programming/atcoder_rust/contest/atcoder/target/debug/deps/abc172_a-b02a3ec13cddfce6: src/bin/a.rs
+
+/Users/MasatoSaito/competitive_programming/atcoder_rust/contest/atcoder/target/debug/deps/abc172_a-b02a3ec13cddfce6.d: src/bin/a.rs
+
+src/bin/a.rs:
---
+++ contest/atcoder/target/debug/deps/abc172_d-7d8035d974a73f4a.d
@@ -0,0 +1,5 @@
+/Users/MasatoSaito/competitive_programming/atcoder_rust/contest/atcoder/target/debug/deps/abc172_d-7d8035d974a73f4a: src/bin/d.rs
+
+/Users/MasatoSaito/competitive_programming/atcoder_rust/contest/atcoder/target/debug/deps/abc172_d-7d8035d974a73f4a.d: src/bin/d.rs
+
+src/bin/d.rs:
